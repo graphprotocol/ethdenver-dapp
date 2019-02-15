ac6713e53091f07bd9a9e9f2810c6f45f89ab5bc
Jannis Pohlmann <contact@jannispohlmann.de>
2019-02-15 18:24:44 +0100

    Error: Improve error rendering

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid, Typography, createStyles, withStyles } from '@material-ui/core'
 
-const Error = ({ error }) => (
+const errorStyles = theme =>
+  createStyles({
+    errorText: {
+      fontFamily: 'Inconsolata, Monaco, monospace',
+    },
+  })
+
+const Error = ({ classes, error }) => (
   <Grid container direction="column">
     <Grid item>
-      <Typography variant="subtitle1">Error</Typography>
+      <Typography variant="h6" component="h3" className={classes.displayName}>
+        Error
+      </Typography>
     </Grid>
     <Grid item>
       <Grid container>
         <Typography mode="paragraph" color="secondary">
-          <pre>{JSON.stringify(error, undefined, 2)}</pre>
+          <pre className={classes.errorText}>{JSON.stringify(error, undefined, 2)}</pre>
         </Typography>
       </Grid>
     </Grid>
   </Grid>
 )
 
-export default Error
+export default withStyles(errorStyles)(Error)
